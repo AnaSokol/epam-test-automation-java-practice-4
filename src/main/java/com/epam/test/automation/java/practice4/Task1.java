8789cc7baadf1caec11298a0ffff34a7bc3a1bdb
HvqEMAOU <hvqemaou@email.com>
2021-04-02 17:12:45 +0000

    Update Task1.java

--- src/main/java/com/epam/test/automation/java/practice4/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task1.java
@@ -1,6 +1,5 @@
 package com.epam.test.automation.java.practice4;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Task1 {
 
@@ -8,15 +7,20 @@ public class Task1 {
         if (array == null || array.length == 0) {
             throw new IllegalArgumentException();
         }
-        int[] tAsc = new int[array.length];
-        tAsc = array;
-        Arrays.sort(tAsc);
-        int[] tDesc = new int[array.length];
-        tDesc = array;
-        Arrays.stream(tDesc).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
-        if (array == tAsc || array == tDesc) {
-            return true;
+        if (order == SortOrder.ASC){
+           for (int i = 1; i < array.length; i++) {
+               if (array[i] < array[i-1]) {
+                   return false;
+               }
+           }
+        }else {
+           for (int i = 1; i < array.length; i++) {
+        	   if (array[i] > array[i-1]) {
+        		   return false;  
+        	   }
+           }
         }
-            return false;
+
+         return true;
     }
 }
