d7e5b383037d43fff300ae9c9020159c5c35ba76
HvqEMAOU <hvqemaou@email.com>
2021-04-02 19:56:01 +0000

    Update Task2.java

--- src/main/java/com/epam/test/automation/java/practice4/Task2.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task2.java
@@ -1,15 +1,14 @@
 package com.epam.test.automation.java.practice4;
 
 public class Task2 {
-
-    /**
-     * <summary>
-     * Implement code according to description of task.
-     * </summary>
-     * if set invalid arguments in method, then method must throws
-     * IllegalArgumentException
-     */
-    public static int[] transform(int[] array, SortOrder order) {
-        throw new UnsupportedOperationException();
-    }
+   
+   public static int[] transform(int[] array, SortOrder order) {		        
+		int[] arrResult = Arrays.copyOf(array, array.length);
+		if (Task1.isSorted(array, SortOrder.ASC) || Task1.isSorted(array, SortOrder.DESC)) {
+		   for (int i=0; i<array.length; i++) {
+		   arrResult[i] = array[i]+i;
+		   }
+		}
+		return arrResult;	   
+	}
 }
