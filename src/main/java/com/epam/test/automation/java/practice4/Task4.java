db05f40a8aad9085626d63ffed406cda65d734a6
HvqEMAOU <hvqemaou@email.com>
2021-04-04 16:30:25 +0000

    Update Task4.java

--- src/main/java/com/epam/test/automation/java/practice4/Task4.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task4.java
@@ -1,15 +1,25 @@
 package com.epam.test.automation.java.practice4;
 
 public class Task4 {
+  
+    private static double sumGeometricElements(int a1, double t, int alim) {
+        if (a1 <= alim) {
+            throw new IllegalArgumentException("First element a1 has to be bigger than alim");
+        }
+		if (t<=0 || t>=1) {
+            throw new IllegalArgumentException("Step t has to be between 0 and 1");
+        }
 
-    /**
-     * <summary>
-     * Implement code according to description of task.
-     * </summary>
-     * if set invalid arguments in method, then method must throws
-     * IllegalArgumentException
-     */
-    public static double sumGeometricElements(int a1, double t, int alim) {
-        throw new UnsupportedOperationException();
-    }
+		double a = a1;
+		double sum = a1;
+		do {
+			a *= t;
+			if (a<=alim) {
+				break;
+			}
+			sum +=a;
+		}
+		while (a > alim);
+		return sum;
+	}
 }
