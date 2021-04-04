7b3ae37693814b4804438b5b8bb576450ebf7f72
HvqEMAOU <hvqemaou@email.com>
2021-04-04 16:52:42 +0000

    Update Task4.java

--- src/main/java/com/epam/test/automation/java/practice4/Task4.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task4.java
@@ -3,23 +3,21 @@ package com.epam.test.automation.java.practice4;
 public class Task4 {
   
     private static double sumGeometricElements(int a1, double t, int alim) {
-	        if (a1 <= alim) {
-	            throw new IllegalArgumentException("First element a1 has to be bigger than alim");
-	        }
-			if (t<=0 || t>=1) {
-	            throw new IllegalArgumentException("Step t has to be between 0 and 1");
-	        }
-
-			double a = a1;
-			double sum = a1;
-			do {
-				a *= t;
-				if (a<=alim) {
-					break;
-				}
-				sum +=a;
+	   if (a1 <= alim) {
+	       throw new IllegalArgumentException("First element a1 has to be bigger than alim");
+	   }
+	   if (t<=0 || t>=1) {
+	       throw new IllegalArgumentException("Step t has to be between 0 and 1");
+	   }
+	    double a = a1;
+	    double sum = a1;
+		while (a > alim) {
+			a *= t;
+			if (a <= alim) {
+				break;
 			}
-			while (a > alim);
-			return sum;
+			sum +=a;
+		}
+		return sum;
 	}
 }
