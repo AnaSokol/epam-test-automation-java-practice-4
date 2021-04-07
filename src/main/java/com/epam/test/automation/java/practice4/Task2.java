fa3ba9c1c1e678ba4396f434b7194b6118f7bd9c
HvqEMAOU <hvqemaou@email.com>
2021-04-07 07:59:19 +0000

    Update Task2.java

--- src/main/java/com/epam/test/automation/java/practice4/Task2.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task2.java
@@ -2,7 +2,7 @@ package com.epam.test.automation.java.practice4;
 
 import java.util.Arrays;
 
-public class Task2 {
+private class Task2 {
    
    public static int[] transform(int[] array, SortOrder order) {		        
 		int[] arrResult = Arrays.copyOf(array, array.length);
